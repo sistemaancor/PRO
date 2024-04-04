5d0d70d3c908536a25cadf4d3d1dada5e8dd9130
sistemaancor <157703932+sistemaancor@users.noreply.github.com>
2024-04-04 11:18:52 +0100

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,17 @@
-PRINT("HOLA MUNDO")
+def hola_mundo(opcion):
+    if opcion == 1:
+        print("Hola mundo")
+    elif opcion == 2:
+        print("Hola mundo!")
+    else:
+        print("Opción no válida")
+
+def main():
+    print("Bienvenido al programa")
+    print("1. Hola mundo")
+    print("2. Hola mundo!")
+    opcion = int(input("Seleccione una opción (1 o 2): "))
+    hola_mundo(opcion)
+
+if __name__ == "__main__":
+    main()
